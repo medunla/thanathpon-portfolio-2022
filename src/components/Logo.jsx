11577522335a85e9f7909bab1001ce657205bdf7
PanupatK <panupatnew@gmail.com>
2022-05-20 19:29:20 +0700

    Change <a> to <Link>

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,12 @@
+import { Link } from 'react-router-dom'
+import { route } from '../enums/route'
+
 const Logo = () => {
 	return (
 		<h1 id="fh5co-logo">
-			<a href="http://thnp-w.com" title="Thanathpon's Portfolio">
-				<img className="custom-logo custom-logo-desktop" src="http://thnp-w.com/wp-content/uploads/2020/12/Asset-3@2x.png" alt="Thanathpon's Portfolio" title="Thanathpon's Portfolio" />
-			</a>
+			<Link to={route.HOME} title="Thanathpon's Portfolio">
+				<img src="http://thnp-w.com/wp-content/uploads/2020/12/Asset-3@2x.png" alt="Thanathpon's Portfolio" title="Thanathpon's Portfolio" />
+			</Link>
 		</h1>
 	)
 }
